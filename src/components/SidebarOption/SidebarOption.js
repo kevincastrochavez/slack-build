9fc86ccc26ba3f0740359b63ff73e9947e09aac4
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-20 10:07:57 -0600

    added redux to the sidebar, created the enterRoom reducer and dispatched the id of the room to the store when clicked

--- src/components/SidebarOption/SidebarOption.js
+++ src/components/SidebarOption/SidebarOption.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 
 import { firestore } from "../../firebase";
+import { enterRoom } from "../../features/appSlice";
 
 import {
   SidebarOptionContainer,
@@ -8,6 +10,8 @@ import {
 } from "./SidebarOption.styles";
 
 function SidebarOption({ Icon, title, addChannelOption, id }) {
+  const dispatch = useDispatch();
+
   const addChannel = () => {
     const channelNme = prompt("Please enter the channel name");
 
@@ -18,7 +22,15 @@ function SidebarOption({ Icon, title, addChannelOption, id }) {
     }
   };
 
-  const selectChannel = () => {};
+  const selectChannel = () => {
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      );
+    }
+  };
 
   return (
     <SidebarOptionContainer
