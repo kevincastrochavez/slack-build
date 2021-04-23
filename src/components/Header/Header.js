7b6da54cd26845010b2a9d3a5154994cda3e4bb5
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-22 21:08:52 -0600

    implemented the ability to signOut

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { AccessTime, HelpOutline, Search } from "@material-ui/icons";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+
+import { auth } from "../../firebase.js";
 
 import {
   HeaderContainer,
@@ -10,10 +15,35 @@ import {
 } from "./Header.styles.jsx";
 
 function Header() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [user] = useAuthState(auth);
+  console.log(user);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <HeaderContainer>
       <HeaderLeft>
-        <HeaderAvatar />
+        <HeaderAvatar
+          src={user?.photoURL}
+          alt={user?.displayName}
+          onClick={handleClick}
+        />
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={() => auth.signOut()}>Logout</MenuItem>
+        </Menu>
         <AccessTime />
       </HeaderLeft>
 
